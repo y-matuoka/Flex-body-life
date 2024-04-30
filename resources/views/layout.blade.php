31301ccc337b1ce9cea9c010b6600f3df62f98a3
sakie-ooyama <tra202312.ooyama@gmail.com>
2024-04-30 14:49:26 +0900

    css共通

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Flex-body-life</title>
+
+@yield('styles')
+@section()
+
+  <link rel="stylesheet" href="/css/styles.css"> 
+  {{-- コンフリクトを避けるためのcssの作り方 --}}
+  {{-- ↑各ページに固有のスタイリングを適用させるには各ページのcssを記載 --}}
+  {{-- 各ページ共通部分のスタイルを描いたCSSファイルを用意する --}}
+  {{-- 各ページのスタイルを描いたCSSファイルを用意する --}}
+  {{-- cssファイルを分けておいたほうがgitの管理が楽 --}}
+  {{-- class名がメンバーで被らないように注意する --}}
+</head>
+
+<body>
+<header>
+ {{-- 一旦空 --}}
+
+</header>
+
+<main>
+@yield('content')
+{{-- ページそれぞれの中身 --}}
+</main>
+<footer>
+  {{-- 一旦空 --}}
+</footer>
+</body>
+</html>
