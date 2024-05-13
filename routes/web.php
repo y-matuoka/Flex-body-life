06cfa8b2a8cc01035248bfaec2e87b5cdbd658e6
y-matuoka <regurusu.8901@gmail.com>
2024-05-13 13:23:55 +0900

    マイページ作成

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,17 @@ Route::get('/mypage', 'MyPageController@index');
 
 Auth::routes();
 
-// 下２つの記述は消してください。
-// views作成時に画面で確認したい為、記述しました。
-// Route::get('auth/register', function(){
-//     return view('auth.register');
-// });
-Route::get('goal_setting/index', function(){
-    return view('goal_setting.index');
+//目標設定ページ
+Route::group(['middleware' => 'auth'],function(){
+    Route::get('/goal_setting/index', 'GoalSettingController@index')->name('goal.index');
+    Route::post('/goal_setting/index',  'GoalSettingController@store')->name('goal.store');
+
+    Route::get('/goal_setting/{id}/update', 'GoalSettingController@edit')->name('goal.edit');
+    Route::post('/goal_setting/{id}/update',  'GoalSettingController@update')->name('goal.update');
+});
+
+
+//目標設定更新ページ。ビューをみるために記載しました
+Route::get('goal_setting/update', function(){
+    return view('goal_setting.update');
 });
