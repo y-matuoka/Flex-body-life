2dc1c4b7619d3fc25fe8f0c1707708afda2c0ebb
y-matuoka <regurusu.8901@gmail.com>
2024-05-20 16:19:24 +0900

    レスポンシブ途中

--- routes/web.php
+++ routes/web.php
@@ -29,30 +29,39 @@ Route::get('/mypage/update', 'MypageUpdateController@show')->name('mypage.update
 Route::post('/mypage/update', 'MypageUpdateController@updateProfile')->name('update.profile');
 
 
-Auth::routes();
+// ログイン状態時にアクセス
+
+
+
 //目標設定ページ
 Route::group(['middleware' => 'auth'],function(){
-    Route::get('/goal_setting/index', 'GoalSettingController@index')->name('goal.index');
+    //目標設定
+    Route::get('/goal_setting//index', 'GoalSettingController@index')->name('goal.index');
     Route::post('/goal_setting/index',  'GoalSettingController@store')->name('goal.store');
-
+     //目標設定変更
     Route::get('/goal_setting/{id}/edit', 'GoalSettingController@edit')->name('goal.edit');
     Route::post('/goal_setting/{id}/edit',  'GoalSettingController@update')->name('goal.update');
-
+    //コース選択
     Route::get('/courses/index', 'CourseController@index')->name('course.index');
     Route::post('/courses/index', 'CourseController@store')->name('course.store');
-
-    
+    //コース選択変更
     Route::get('/courses/{id}/edit', 'CourseController@edit')->name('course.edit');
     Route::post('/courses/{id}/edit', 'CourseController@update')->name('course.update');
-    Route::get('/courses/{id}/updated', 'CourseController@show')->name('courses.updated');
 
-  
+
+    //コース変更完了しました画面
+    // Route::get('/courses/{id}/updated', 'CourseController@show')->name('courses.updated');
 });
+
+Route::get('training/index', function(){
+    return view('training.index');
+});
+
 Route::get('goal_setting/index', function(){
     return view('goal_setting.index');
 });
 // 5/8追加views作成時に画面で確認したい為、記述しました。
-Route::get('auth/trainingmenu', function(){
+Route::get('/auth/trainingmenu', function(){
     return view('auth.trainingmenu');
 });
 // 5/10追加views作成時に画面で確認したい為、記述しました。
@@ -73,6 +82,14 @@ Route::get('/auth/muscle', function () {
     return view('auth.muscle');
 })->name('auth.muscle');
 Route::get('/auth/stretch', function () {
+    return view('auth.stretch');
+})->name('auth.stretch');
+
+
+
+
+
+Route::get('/training/index', function(){
     return view('training/index');
 })->name('training.index');
 
@@ -80,7 +97,17 @@ Route::get('/auth/stretch', function () {
 Route::get('goal_setting/update', function(){
     return view('goal_setting.update');
 });
-// 5/15　musclepageからmypageに行くためのルーティング
+
+// 5/15musclepageからmypageに行くためのルーティング
+
 Route::get('mypage', function () {
     return view('mypage');
 })->name('mypage');
+
+// 5/15　musclepageからmypageに行くためのルーティング
+// Route::get('mypage', function () {
+//     return view('mypage');
+// })->name('mypage');
+
+// 5/17rimainderモック確認ビューの作成です
+Route::get('auth/{user}/reminder','ReminderController@index' )->name('reminder');
